5a8bb89a4ec0c2991b361a3957b4ab6eaf6075aa
JeanJuba <jeanwsj96@hotmail.com>
2024-10-24 23:47:54 -0300

    Rename main class

--- src/main/java/com/juba/spring_boot_library/SpringBooJwtAuthApplication.java
+++ src/main/java/com/juba/spring_boot_library/SpringBooJwtAuthApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootLibraryApplication {
+public class SpringBooJwtAuthApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootLibraryApplication.class, args);
+		SpringApplication.run(SpringBooJwtAuthApplication.class, args);
 	}
 
 }
--- src/test/java/com/juba/spring_boot_library/SpringBooJwtAuthApplicationTests.java
+++ src/test/java/com/juba/spring_boot_library/SpringBooJwtAuthApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBootLibraryApplicationTests {
+class SpringBooJwtAuthApplicationTests {
 
 	@Test
 	void contextLoads() {
